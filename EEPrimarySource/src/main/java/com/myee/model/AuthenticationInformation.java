0c129363aa92904c0fd778df9d7bd63d3c73c159
tiwari.pankaj@gmail.com <tiwari.pankaj@gmail.com>
2015-02-01 23:40:10 +0530

    Some change

---
+++ EEPrimarySource/src/main/java/com/myee/model/AuthenticationInformation.java
@@ -0,0 +1,34 @@
+package com.myee.model;
+
+/**
+ * Created by pankaj on 01-02-2015.
+ */
+public class AuthenticationInformation {
+    private String userName;
+    private String password;
+    private String errorMessage;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
---
+++ EEejbSession/src/main/java/com/myee/service/AuthenticationBean.java
@@ -0,0 +1,20 @@
+package com.myee.service;
+
+import com.myee.model.AuthenticationInformation;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by pankaj on 01-02-2015.
+ */
+@Stateless(name = "AuthenticationEJB")
+public class AuthenticationBean {
+    public AuthenticationBean() {
+    }
+    public boolean authenticate (AuthenticationInformation authenticationInformation){
+        if(authenticationInformation.getPassword().equals(authenticationInformation.getPassword())){
+            return true;
+        }
+        return false;
+    }
+}
