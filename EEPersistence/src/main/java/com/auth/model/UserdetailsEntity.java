466d9cc091f89d091ca3b74835aa03ab89b5b947
tiwari.pankaj@gmail.com <tiwari.pankaj@gmail.com>
2015-05-26 08:02:55 +0530

    Test

---
+++ EEPersistence/src/main/java/com/auth/model/UserdetailsEntity.java
@@ -0,0 +1,66 @@
+package com.auth.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by pankaj on 24-05-2015.
+ */
+@Entity
+@Table(name = "userdetails", schema = "", catalog = "assign")
+public class UserdetailsEntity {
+    private String userId;
+    private String userPassword;
+    private int id;
+
+    @Basic
+    @Column(name = "userId", nullable = true, insertable = true, updatable = true, length = 20)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "userPassword", nullable = true, insertable = true, updatable = true, length = 20)
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserdetailsEntity that = (UserdetailsEntity) o;
+
+        if (id != that.id) return false;
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (userPassword != null ? !userPassword.equals(that.userPassword) : that.userPassword != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (userPassword != null ? userPassword.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
+    }
+}
--- EEejbSession/src/main/java/com/myee/service/AuthenticationBean.java
+++ EEejbSession/src/main/java/com/myee/service/AuthenticationBean.java
@@ -1,17 +1,31 @@
 package com.myee.service;
 
+import com.auth.model.UserdetailsEntity;
 import com.myee.model.AuthenticationInformation;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by pankaj on 01-02-2015.
  */
 @Stateless(name = "AuthenticationEJB")
 public class AuthenticationBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public AuthenticationBean() {
     }
     public boolean authenticate (AuthenticationInformation authenticationInformation){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<UserdetailsEntity> criteriaQuery = criteriaBuilder.createQuery(UserdetailsEntity.class);
+        Root<UserdetailsEntity> userdetailsEntityRoot=criteriaQuery.from(UserdetailsEntity.class);
+        criteriaQuery.select(userdetailsEntityRoot).where(criteriaBuilder.equal(userdetailsEntityRoot.get("userId"),authenticationInformation.getUserName()));
         if(authenticationInformation.getPassword().equals(authenticationInformation.getPassword())){
             authenticationInformation.setGetNextPage("success");
             return true;
